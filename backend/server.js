cbd732c45d798e38d56bb2810b26f60a0ae8171f
SpringMan-Sonic <springman9999@gmail.com>
2026-02-17 15:02:55 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected Successfully'))
+  .catch(err => console.error('MongoDB connection error:', err.message));
 
 
 app.use('/api/auth', authRoutes);
